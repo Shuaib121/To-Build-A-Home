3c1c6487b8fdb6c8ee0558beff096177c5ca83f7
Shuaib Ebrahim <ebrahimshuaib1@gmail.com>
2021-10-27 21:28:46 +0200

    Moved files and Fixed movement bug

---
+++ Assets/Scripts/Gameplay/CharController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 4f;
+
+    Vector3 forward, right;
+    bool isFaceMethodRunning = false;
+
+    void Start()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("VerticalKey") != 0 || Input.GetAxis("HorizontalKey") != 0)
+            Move();
+    }
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0f, Input.GetAxis("VerticalKey"));
+
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        if (!isFaceMethodRunning)
+        {
+            StartCoroutine(FaceDirection(heading));
+        }
+
+        transform.position += rightMovement;
+        transform.position += upMovement;
+    }
+
+    IEnumerator FaceDirection(Vector3 heading)
+    {
+        isFaceMethodRunning = true;
+
+        yield return new WaitForSeconds(0.05f);
+
+        isFaceMethodRunning = false;
+
+        if (Input.GetAxis("VerticalKey") != 0 || Input.GetAxis("HorizontalKey") != 0)
+        {
+            transform.forward = heading;
+        }
+    }
+}
+
+
