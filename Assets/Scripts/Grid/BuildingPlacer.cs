f999dd6c4070c281150ecc45d8ef103acf7704a5
Shuaib Ebrahim <ebrahimshuaib1@gmail.com>
2021-10-28 19:34:25 +0200

    Added grid placement

---
+++ Assets/Scripts/Grid/BuildingPlacer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPlacer : MonoBehaviour
+{
+    public GameObject test;
+
+    private bool currentlyPlacing;
+    private BuildingPreset curBuildingPreset;
+
+    private float placementIndicatorUpdateRate = 0.05f;
+    private float lastUpdateTime;
+    private Vector3 curPlacementPos;
+
+    public GameObject placementIndicator;
+
+    public static BuildingPlacer inst;
+
+    void Awake()
+    {
+        inst = this;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+            CancelBuildingPlacement();
+
+        if(Time.time - lastUpdateTime > placementIndicatorUpdateRate && currentlyPlacing)
+        {
+            lastUpdateTime = Time.time;
+
+            curPlacementPos = Selector.inst.GetCurTilePosition();
+            placementIndicator.transform.position = curPlacementPos;
+        }
+
+        if(currentlyPlacing && Input.GetMouseButtonDown(0))
+        {
+            PlaceBuilding();
+        }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            BeginNewBuildingPlacement();
+        }
+    }
+
+    public void BeginNewBuildingPlacement()
+    {
+        currentlyPlacing = true;
+        //curBuildingPreset = buildingPreset;
+        placementIndicator.SetActive(true);
+    }
+
+    public void CancelBuildingPlacement()
+    {
+        currentlyPlacing = false;
+        placementIndicator.SetActive(false);
+    }
+
+    void PlaceBuilding()
+    {
+        GameObject buildingObj = Instantiate(test, curPlacementPos, Quaternion.identity);
+
+        CancelBuildingPlacement();
+    }
+}
---
+++ Assets/Scripts/Grid/Selector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Selector : MonoBehaviour
+{
+    private Camera cam;
+    public static Selector inst;
+    public GameObject test;
+
+    void Awake()
+    {
+        inst = this;
+    }
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    public Vector3 GetCurTilePosition()
+    {
+        /*if(EventSystem.current.IsPointerOverGameObject())
+            return new Vector3(0, -99, 0);*/
+
+        Vector3 fwd = test.transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(test.transform.position, fwd, out hit, 10))
+            print("There is something in front of the object!");
+
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+        Ray ray = cam.ScreenPointToRay(cam.WorldToScreenPoint(hit.point));
+
+        float rayOut = 0.0f;
+
+        if(plane.Raycast(cam.ScreenPointToRay(cam.WorldToScreenPoint(hit.point)), out rayOut))
+        {
+            Vector3 newPos = ray.GetPoint(rayOut) - new Vector3(0.5f, 0.0f, 0.5f);
+            return new Vector3(Mathf.CeilToInt(newPos.x), 0, Mathf.CeilToInt(newPos.z));
+        }
+
+        return new Vector3(0, -99, 0);
+    }
+}
